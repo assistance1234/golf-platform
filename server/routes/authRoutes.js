6127ff6948fc6ad8016b7431e486ebe14e3318f2
Lenovo <sharmasrijan09@gmail.com>
2026-04-22 22:15:17 +0530

    added auth test route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,6 +3,11 @@ import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
+// ✅ test route add kar
+router.get("/", (req, res) => {
+  res.send("Auth route working ✅");
+});
+
 router.post("/register", register);
 router.post("/login", login);
 
